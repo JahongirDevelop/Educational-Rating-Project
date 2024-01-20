9e09a9d274285e38d402cab701213f31d82d4f4b
Jahongir <sjahongir22@gmail.com>
2024-01-20 22:55:23 +0500

    w

--- src/main/java/soqqa/com/ratingproject/controller/WorkController.java
+++ src/main/java/soqqa/com/ratingproject/controller/WorkController.java
@@ -3,9 +3,11 @@ package soqqa.com.ratingproject.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import soqqa.com.ratingproject.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/work")
 public class WorkController {
+    private final UserService userService;
 }
